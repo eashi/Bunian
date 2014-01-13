7eeb157d8d13d80d8e9b3051f7f002c28cf3ae56
Emad Alashi <emad.ashi@gmail.com>
2014-01-13 11:18:47 +1100

    added first scripts for database upgrade

--- UpgradeDatabase/Program.cs
+++ UpgradeDatabase/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DbUp;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,31 @@ namespace UpgradeDatabase
     {
         static void Main(string[] args)
         {
+            var connectionString = ConfigurationManager.ConnectionStrings[args[0]].ToString();
+
+            var upgrader = DeployChanges
+                .To
+                .SqlDatabase(connectionString)
+                .WithScriptsFromFileSystem(".\\Scripts")
+                .LogToConsole()
+                .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if(result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Database updated successfully");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Database update failed");
+                Console.ResetColor();
+                Environment.ExitCode = 1;
+            }
+
         }
     }
 }
---
+++ UpgradeDatabase/Scripts/0001- Create Sponsors table.sql
@@ -0,0 +1,13 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Sponsors]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [dbo].[Sponsors](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[Phone] [int] NOT NULL,
+ CONSTRAINT [PK_Sponsors] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
