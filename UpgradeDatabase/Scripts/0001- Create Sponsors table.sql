ba0d1723fccc82ce2701831568566a5ca005289d
Emad Alashi <emad.ashi@gmail.com>
2014-01-14 14:39:03 +1100

    change the create script to work on Azure

--- UpgradeDatabase/Scripts/0001- Create Sponsors table.sql
+++ UpgradeDatabase/Scripts/0001- Create Sponsors table.sql
@@ -1,13 +1,10 @@
 ﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Sponsors]') AND type in (N'U'))
 BEGIN
 CREATE TABLE [dbo].[Sponsors](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Id] [int] IDENTITY(1,1) NOT NULL Primary Key,
 	[Name] [nvarchar](50) NOT NULL,
 	[Phone] [int] NOT NULL,
- CONSTRAINT [PK_Sponsors] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX  = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
+ 
+)
 END
 GO
